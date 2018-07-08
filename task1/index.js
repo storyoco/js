4f54f06a8c6f127b778f7c40a7ecc4fb81894675
storyoco <39665472+storyoco@users.noreply.github.com>
2018-07-08 14:45:25 +0800

    Add files via upload

--- task1/index.js
+++ task1/index.js
@@ -5,67 +5,53 @@ window.onload = function () {
     var cube = document.getElementsByClassName('cube');
     var mycolor;
 
-
+    //清除颜色
+    function clearColor() {
+        for (i=0; i<cube.length; i++) {
+            cube[i].style.backgroundColor = '#fea500';
+        }
+    }
 
     //随机格子颜色
     function random_color() {
-        var test = [0,1,2,3,4,5,6,7,8];
+
+        //清除颜色
+        clearColor();
 
         //洗牌算法取随机格子
-        Array.prototype.shuffle = function() {
-            var input = this;
-
-            for (var i = input.length-1; i >=0; i--) {
-
-                var randomIndex = Math.floor(Math.random()*(i+1));
-                var itemAtIndex = input[randomIndex];
-
-                input[randomIndex] = input[i];
-                input[i] = itemAtIndex;
+        function cubeShuffle() {
+            //取格子数组
+            var test = new Array(cube.length);
+            for (i=0; i<test.length; i++){
+                test[i] = i;
             }
-            return input;
-        };
-        test.shuffle();
-
-        //条件判断取随机格子
-        /*var arr_3 = [];
-        function numRandom() {
-            var random = Math.floor(Math.random()*9);
-            if(arr_3.length < 3) {
-                for(i=0;i<=arr_3.length;i++) {
-                    if( random == arr_3[i] ) {
-                        break;
-                    }
-                    else {
-                        if( i == arr_3.length ) {
-                            arr_3.push(random);
-                            break;
-                        }
-                    }
+            //洗牌算法
+            Array.prototype.shuffle = function() {
+                var input = this;
+                for (var i = input.length-1; i >=0; i--) {
+                    var randomIndex = Math.floor(Math.random()*(i+1));
+                    var itemAtIndex = input[randomIndex];
+                    input[randomIndex] = input[i];
+                    input[i] = itemAtIndex;
                 }
-                numRandom();
-            }
+                return input;
+            };
+            test.shuffle();
+            return test.shuffle();
         }
-        numRandom();*/
 
         //随机颜色
-        var arr_r = [Math.floor(Math.random()*256),Math.floor(Math.random()*256),Math.floor(Math.random()*256)];
-        var arr_g = [Math.floor(Math.random()*256),Math.floor(Math.random()*256),Math.floor(Math.random()*256)];
-        var arr_b = [Math.floor(Math.random()*256),Math.floor(Math.random()*256),Math.floor(Math.random()*256)];
-        var arr_rgb = [arr_r,arr_g,arr_b];
-        //清除颜色
-        for (j=0; j<cube.length; j++) {
-            cube[j].style.backgroundColor = '#fea500';
+        function rgb() {
+            var r = Math.floor(Math.random()*255);
+            var g = Math.floor(Math.random()*255);
+            var b = Math.floor(Math.random()*255);
+            return 'rgb(' + r + "," + g + ","+ b + ')';
         }
+
         //变化格子颜色
-        //洗牌法
-        for (i=0; i<3; i++){
-            cube[test[i]].style.backgroundColor = "rgb("+arr_rgb[i][0]+","+arr_rgb[i][1]+","+arr_rgb[i][2]+")";
+        for (n=0; n<3; n++){
+            cube[cubeShuffle()[n]].style.backgroundColor = rgb();
         }
-        //条件判断法
-        /* for (i=0; i<arr_3.length; i++) {
-             cube[arr_3[i]].style.backgroundColor = "rgb("+arr_rgb[i][0]+","+arr_rgb[i][1]+","+arr_rgb[i][2]+")";
-        }*/
     }
 
     //开始闪
@@ -89,9 +75,7 @@ window.onload = function () {
         start.style.backgroundColor = 'white';
         start.style.color = '#fea500';
         //清除颜色
-        for (i=0; i<cube.length; i++) {
-            cube[i].style.backgroundColor = '#fea500';
-        }
+        clearColor();
         //清除定时器
         clearInterval(mycolor)
     };
